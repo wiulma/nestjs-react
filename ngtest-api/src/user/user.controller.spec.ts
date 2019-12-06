aefcaec965abd55fc5ab03f7b4b0f97e7b25b655
Giorgio Zett <giorgio.zett@gmail.com>
2019-12-06 18:28:28 +0100

    add small test

--- ngtest-api/src/user/user.controller.spec.ts
+++ ngtest-api/src/user/user.controller.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('Users Controller', () => {
-  let controller: UserController;
+	let userController: UserController;
+	let userService: UserService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [UserController],
+			providers: [UserService]
+		}).compile();
 
-    controller = module.get<UserController>(UserController);
-  });
+		userService = module.get<UserService>(UserService);
+		userController = module.get<UserController>(UserController);
+	});
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(userController).toBeDefined();
+	});
+
+	describe('findAll', () => {
+		it('should return an array of users', async () => {
+			const result = [{id: 1, name: 'giorgio'}];
+			jest.spyOn(userService, 'findAll').mockImplementation(() => Promise.resolve(result));
+			expect(await userController.findAll()).toBe(result);
+		});
+
+	});
 });
